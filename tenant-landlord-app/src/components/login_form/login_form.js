286ea0eefa9685ce33f965aaee1ed4e9752f7e96
Polaris106 <wangxingrui2134@gmail.com>
2023-06-13 14:03:19 +0800

    added signup and forget-password

--- tenant-landlord-app/src/components/login_form/login_form.js
+++ tenant-landlord-app/src/components/login_form/login_form.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./login_form.css"
 import "bootstrap/dist/css/bootstrap.min.css";
+import {Routes, Route, useNavigate, Link} from 'react-router-dom';
 
 const LoginForm = () => {
 
@@ -10,8 +11,12 @@ const LoginForm = () => {
                 <h1>Welcome!</h1>
                 <input type="text" placeholder="USERNAME" />
                 <input type="password" placeholder="PASSWORD" />
+                
 
                 <div className="login-btn">LOGIN</div>
+                <div className="sign-up">Don't have an account? <Link className="sign-up-link">Sign up!</Link></div>
+                <Link className="password-reset">forget password?</Link>
+                
             </div>
         </div>
 
