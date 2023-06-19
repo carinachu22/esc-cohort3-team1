c6e4efe8df0460de3c27d28d48f39edb4abf8c98
Polaris106 <wangxingrui2134@gmail.com>
2023-06-20 00:09:51 +0800

    made some structural changes

--- tenant-landlord-app/src/pages/App.js
+++ tenant-landlord-app/src/pages/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import LandlordLogin from './landlord_login';
 import TenantLogin from './tenant_login';
 import LandlordSignup from './landlord_signup';
-import TenantSignup from './tenant_signup';
 import Dashboard from './Dashboard';
 
 import {Routes, Route} from 'react-router-dom';
@@ -17,7 +16,6 @@ function App() {
           <Route path="/pages/tenant_login/*" element={<TenantLogin />} />
           <Route path="/pages/landlord_login/*" element={<LandlordLogin />} />
           <Route path="/pages/landlord_signup/*" element={<LandlordSignup />} />
-          <Route path="/pages/tenant_signup/*" element={<TenantSignup />} />
           <Route path='/pages/Dashboard/*' element={<Dashboard/>} />
         </Routes>
       </div>
--- tenant-landlord-app/src/pages/landlord_signup.js
+++ tenant-landlord-app/src/pages/landlord_signup.js
@@ -1,12 +1,49 @@
-import React from "react";
-import SignupLandlord from "../components/signup_forms/signup_form_landlord";
+import React, { useState } from "react";
+import SignupStyles from "../styles/signup_form_landlord.module.css";
+import PasswordStyles from "../styles/usePasswordToggle.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {useNavigate} from 'react-router-dom';
 
 const LandlordSignup = () => {
-	return (
-		<div className="page">
-			<SignupLandlord />
-		</div>
-	);
-};
+  const navigate = useNavigate();
 
-export default LandlordSignup;
+  const [passwordShown, setPasswordShown] = useState(false);
+
+  const togglePassword = () => {
+      //passwordShown = true if handler is invoked  
+      setPasswordShown(!passwordShown)
+    }
+  
+  const navigateToDashboard = () => {
+    navigate('/pages/Dashboard');
+  };
+
+  return (
+      <div className={SignupStyles.page}>
+          <div className={SignupStyles.cover}>
+              <h1 className={SignupStyles.header}>Register</h1>
+              <div className={SignupStyles.context}>sign up as a landlord</div>
+              <input type="text" className={SignupStyles.input} placeholder="USERNAME" />
+              <input type="email" className={SignupStyles.input} placeholder="EMAIL" />
+              <input type="text" className={SignupStyles.input} placeholder="ORGANISATION" />
+              <div className={PasswordStyles.passwordToggle}>
+                <input 
+                    type={passwordShown ? "text" : "password"} 
+                    placeholder="PASSWORD"
+                    className={PasswordStyles.passwordInput}
+                />
+                <span onClick={togglePassword}>
+                    {passwordShown ? "Hide" : "Show"}
+                </span>
+              </div>
+              <div className={SignupStyles.next_btn} onClick={navigateToDashboard}>FINISH</div>
+
+
+          </div>
+      </div>
+
+  
+  )
+}
+
+export default LandlordSignup
