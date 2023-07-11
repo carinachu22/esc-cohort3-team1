2de4210660ea0f97352fce1f9a5861674e90d0a9
Jovi Tan <xxpurelistxx@gmail.com>
2023-07-11 23:00:58 +0800

    Revert "Merge branch 'Dhruv' into carina_backend_integration"
    This reverts commit 0f8031d74c9366a7c1a7ae6bd845dee60a8ddfab, reversing
    changes made to 8554f361b794e6513a0c9be31ac7e6e2ff9675e4.

--- tenant-landlord-app/src/pages/CreateTicketPage.js
+++ tenant-landlord-app/src/pages/CreateTicketPage.js
@@ -1,11 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import axios from 'axios';
 
 import { useAuthUser, useAuthHeader } from 'react-auth-kit';
 import {useFormik} from "formik";
@@ -19,15 +17,9 @@ function CreateTicketPage() {
   const [tenantComment, setTenantComment] = useState('');
   const [ticketType, setTicketType] = useState('');
 
-
-  const handleCreateTicket = async (values) => {
-    try {
-      const response = await axios.post(
-        'http://localhost:5000/api/tenant/createTicket ', 
-        { request_description: values.tenantComment, location: values.location, request_type: values.category }
-      );
-
-      console.log(response.data);
+  const handleCommentChange = (event) => {
+    setTenantComment(event.target.value);
+  };
 
   const handleTicketTypeChange = (event) => {
     setTicketType(event.target.value);
@@ -112,11 +104,14 @@ function CreateTicketPage() {
         Create A Service Ticket
       </Typography>
 
-      <Formik
-        initialValues={{ request_description: '', location: '', request_type: '' }} 
-        onSubmit={handleCreateTicket}
+      {/* Comment Boxes */}
+      <Box
+        display="flex"
+        flexDirection="row"
+        justifyContent="center"
+        alignItems="center"
+        marginBottom="2em"
       >
-          
         {/* Comment Box 1 */}
         <Box
           sx={{
@@ -147,7 +142,6 @@ function CreateTicketPage() {
               },
             }}
           />
-
           <Typography variant="h5" gutterBottom>
             Category Of Request
           </Typography>
@@ -171,64 +165,62 @@ function CreateTicketPage() {
           />
         </Box>
 
-            {/* Comment Box 3 */}
-            <Box
-              display="flex"
-              flexDirection="column"
-              alignItems="flex-start"
-              width="35em"
-              padding="1em"
-              marginLeft="2em"
-            >
-              <Typography variant="h5" gutterBottom>
-                Description
-              </Typography>
-              <Field
-                as={TextField}
-                label="Your Comment"
-                multiline
-                rows={8}
-                variant="outlined"
-                fullWidth
-                name="tenantComment"
-                sx={{
-                  '& .MuiOutlinedInput-root': {
-                    border: '2px solid gray',
-                    backgroundColor: 'rgb(229, 226, 226)',
-                    borderRadius: '0.25em',
-                    paddingLeft: '0.5em',
-                    marginTop: '0em',
-                  },
-                }}
-              />
-            </Box>
-          </Box>
-
-          {/* Submit Ticket Button */}
-          <Box marginBottom="2em">
-            <Button
-              variant="contained"
-              color="primary"
-              type="submit"
-              style={{
-                width: '15em',
-                height: '3em',
-                backgroundColor: 'gray',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                transition: '0.5s',
-                marginTop: '3em',
+        {/* Comment Box 3 */}
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="flex-start"
+          width="35em"
+          padding="1em"
+          marginLeft="2em"
+        >
+          <Typography variant="h5" gutterBottom>
+            Description
+          </Typography>
+          <TextField
+            label="Your Comment"
+            multiline
+            rows={8}
+            variant="outlined"
+            fullWidth
+            value={tenantComment}
+            onChange={handleCommentChange}
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                border: '2px solid gray',
+                backgroundColor: 'rgb(229, 226, 226)',
                 borderRadius: '0.25em',
-                cursor: 'pointer',
-                color: 'white',
-              }}
-            >
-              Submit
-            </Button>
-          </Box>
-        </Form>
-      </Formik>
+                paddingLeft: '0.5em',
+                marginTop: '0em',
+              },
+            }}
+          />
+        </Box>
+      </Box>
+
+      {/* Submit Ticket Button */}
+      <Box marginBottom="2em">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleCreateTicket}
+          style={{
+            width: '15em',
+            height: '3em',
+            backgroundColor: 'gray',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            transition: '0.5s',
+            marginTop: '3em',
+            borderRadius: '0.25em',
+            cursor: 'pointer',
+            color: 'white',
+          }}
+        >
+          Submit
+        </Button>
+      </Box>
     </Box>
   );
 }
