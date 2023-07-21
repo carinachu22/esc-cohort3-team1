1cd83b7178f2316088ef56dcf4e179991138d500
Jovi Tan <xxpurelistxx@gmail.com>
2023-07-21 17:02:54 +0800

    fix: get pdf based on ticket ID and show all pages

--- tenant-landlord-app/src/pages/QuotationPage.js
+++ tenant-landlord-app/src/pages/QuotationPage.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import { Box, Button, useToast, Heading } from '@chakra-ui/react';
 import { ArrowBackIcon } from '@chakra-ui/icons';
 import { useNavigate } from 'react-router-dom';
 
 import NavigationBar from '../components/NavigationBar';
+import { SelectedTicketContext } from '../components/SelectedTicketContext';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function QuotationPage() {
-    const ticketName = "Ticket 1"; // placeholder
+    const {selectedTicket, setSelectedTicket} = useContext(SelectedTicketContext);
+    const ticketName = `${selectedTicket.id}`; // placeholder
+    const [pdfUrl,setPdfUrl] = useState('')
     const navigate = useNavigate();
     const toast = useToast();
 
@@ -35,15 +38,28 @@ function QuotationPage() {
         });
     };
 
+    useEffect(() => {
+        fetch(`http://localhost:5000/api/landlord/getQuotation/?id=${selectedTicket.id}`,
+        ) // Replace with the actual backend URL serving the PDF
+          .then((response) => response.blob())
+          .then((data) => {
+            console.log('data',data)
+            const pdfBlobUrl = URL.createObjectURL(data);
+            setPdfUrl(pdfBlobUrl);
+          })
+          .catch((error) => {
+            console.error(error);
+            // Handle error
+          });
+      }, []);
+
     return (
         <>
         {NavigationBar()}
         <Box p={10} bg="#EDF2F7" borderRadius="md" boxShadow="lg" mr = "10%" ml="10%">
             <Heading mb={5} textAlign="center">Job Quote for Ticket: {ticketName}</Heading>
             <Box display="flex" flexDirection="column" justifyContent="center" minHeight="50vh">
-            <Document file="/lab7.pdf">
-                <Page pageNumber={1} />
-            </Document>
+                {pdfUrl && <iframe src={pdfUrl} width="100%" height="600px" />}
             <Box display="flex" justifyContent="space-around" m={1} p={1}>
                 <Button leftIcon={<ArrowBackIcon />} colorScheme="teal" variant="outline" onClick={() => navigate(-1)}>
                 Back
