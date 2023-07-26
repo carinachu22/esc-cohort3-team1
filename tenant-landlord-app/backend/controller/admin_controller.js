45a5c7bc4503285c7ea4baf5690292350fe507bb
Carina <carinachu22@gmail.com>
2023-07-27 02:16:36 +0800

    feat: Added basic admins account APIs

--- tenant-landlord-app/backend/controller/admin_controller.js
+++ tenant-landlord-app/backend/controller/admin_controller.js
@@ -1,60 +1,95 @@
 import {
-    createLandlord,
-    getAdminByEmail,
-
-  
-  } from "../models/admin_model.js";
+  createAdmin,
+  createLandlord,
+  getAdminByEmail,
+  getLandlordByEmail,
+  deleteLandlordByEmail,
+  createBuilding,
+} from "../models/admin_model.js";
 import { genSaltSync, hashSync, compareSync } from "bcrypt";
 import jwt from "jsonwebtoken";
 import nodemailer from "nodemailer";
 import path from "path";
 import fs from "fs";
-import formidable from 'formidable';
+import formidable from "formidable";
 import { send } from "process";
-  
-  
+
+/**
+ * Create Admin account
+ * @param {*} req email, password(unhashed)
+ * @param {*} res
+ */
+export const controllerCreateAdmin = (req, res) => {
+  const body = req.body;
+  const email = body.email;
+  getAdminByEmail(body.email, (err, result) => {
+    if (!result) {
+      console.log(body);
+      const salt = genSaltSync(10);
+      body.password = hashSync(body.password, salt);
+      createAdmin(body, (err, results) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            success: 0,
+            message: "Database connection error",
+          });
+        }
+        return res.status(200).json({
+          success: 1,
+          message: "created successfully",
+          data: results,
+        });
+      });
+    } else {
+      return res.status(500).json({
+        success: 0,
+        message: "duplicate email",
+      });
+    }
+  });
+};
+
 /**
  * Create landlord account
  * @param {*} req email, password(unhashed), ticket_type
- * @param {*} res 
+ * @param {*} res
  */
 export const controllerCreateLandlord = (req, res) => {
-const body = req.body;
-createLandlord(body, (err, result) => {
+  const body = req.body;
+  const email = body.email;
+  getLandlordByEmail(body.email, (err, result) => {
     if (!result) {
-    console.log(body);
-    const salt = genSaltSync(10);
-    body.password = hashSync(body.password, salt);
-    createLandlord(body, (err, results) => {
+      console.log(body);
+      const salt = genSaltSync(10);
+      body.password = hashSync(body.password, salt);
+      createLandlord(body, (err, results) => {
         if (err) {
-        console.log(err);
-        return res.status(500).json({
+          console.log(err);
+          return res.status(500).json({
             success: 0,
             message: "Database connection error",
-        });
+          });
         }
         return res.status(200).json({
-        success: 1,
-        message: "created successfully",
-        data: results,
+          success: 1,
+          message: "created successfully",
+          data: results,
         });
-    });
+      });
+    } else {
+      return res.status(500).json({
+        success: 0,
+        message: "duplicate email",
+      });
     }
-    else {
-    return res.status(500).json({
-    success: 0,
-    message: "duplicate email",
-
-    });}
-})
-
+  });
 };
 
-
-  /**
+/**
  * Login for Admin
  * @param {*} req Admin email
- * @param {*} res 
+ * @param {*} res
  */
 export const controllerLoginAdmin = (req, res) => {
   const body = req.body;
@@ -92,4 +127,45 @@ export const controllerLoginAdmin = (req, res) => {
   });
 };
 
+export const controllerDeleteLandlordByEmail = (req, res) => {
+  const body = req.body;
+  console.log(body);
+  const { email } = body;
+  console.log(email);
+  deleteLandlordByEmail(email, (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({
+        success: 0,
+        message: "Database connection error",
+      });
+    }
+    return res.status(200).json({
+      success: 1,
+      message: "deleted successfully",
+    });
+  });
+};
 
+/**
+ * Create Building
+ * @param {*} req public_building_id(RC, FC), building_name, address,postal_code
+ * @param {*} res
+ */
+export const controllerCreateBuilding = (req, res) => {
+  const body = req.body;
+  createBuilding(body, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({
+        success: 0,
+        message: "Database connection error",
+      });
+    } else {
+      return res.status(200).json({
+        success: 1,
+        data: results,
+      });
+    }
+  });
+};
