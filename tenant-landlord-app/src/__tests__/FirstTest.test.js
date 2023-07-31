5496d1d86650a9e8e965c4a6c0f561a14a2de881
Carina <carinachu22@gmail.com>
2023-07-31 16:11:28 +0800

    test: mocked react-auth-kit & render ticketList and ViewTicketPage

--- tenant-landlord-app/src/__tests__/FirstTest.test.js
+++ tenant-landlord-app/src/__tests__/FirstTest.test.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import FirstTest from "../component_test/FirstTest.js";
-//import CreateTicketPage from "../pages/CreateTicketPage";
 import { BrowserRouter } from "react-router-dom";
 import App from "../pages/App.js";
 import "@testing-library/jest-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AuthProvider, useSignIn } from "react-auth-kit";
 import LoginPage from "../pages/LoginPage.js";
+import TicketList from "../pages/TicketList.js";
+import ViewTicketPage from "../pages/ViewTicketPage.js";
 
 // Mock react-router-dom before rendering the component
 jest.mock("react-router-dom");
+jest.mock("react-auth-kit");
 
 describe("Level 0: Rendering pages", () => {
   test("Testing the set up file", () => {
@@ -48,10 +50,33 @@ describe("Level 1: Rendering pages", () => {
   });
 });
 
-//async function setup() {
-//SignIn
-// 1. Specify values, username, password
-// 2. API call w defined values
-// 3. Sign in using react auth kit
-
-//}
+describe("Level 2: Rendering pages that requires being logged in", () => {
+  test("Render Ticket List", () => {
+    render(
+      <AuthProvider
+        authType={"cookie"}
+        authName={"_auth"}
+        cookieDomain={window.location.hostname}
+        cookieSecure={false}
+      >
+        <BrowserRouter>
+          <TicketList />
+        </BrowserRouter>
+      </AuthProvider>
+    );
+  });
+  test("Render ViewTicketPage", () => {
+    render(
+      <AuthProvider
+        authType={"cookie"}
+        authName={"_auth"}
+        cookieDomain={window.location.hostname}
+        cookieSecure={false}
+      >
+        <BrowserRouter>
+          <ViewTicketPage />
+        </BrowserRouter>
+      </AuthProvider>
+    );
+  });
+});
