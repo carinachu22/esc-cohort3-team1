987feca99a44801a5d953f100f77bdb7255f8b0f
Carina <carinachu22@gmail.com>
2023-07-31 22:55:22 +0800

    test: added a chakra custom render

---
+++ tenant-landlord-app/src/__tests__/setupTests.js
@@ -0,0 +1,8 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { render } from '@testing-library/react';
+
+const customRender = (ui, options) =>
+  render(ui, { wrapper: ChakraProvider, ...options });
+
+export * from "@testing-library/react";
+export { customRender as render };
