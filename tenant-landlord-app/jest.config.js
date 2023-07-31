be57932cd54192bb86b32338c96180ce5fb57919
Jovi Tan <xxpurelistxx@gmail.com>
2023-07-31 12:35:29 +0800

    fix & refactor: jest supports both ES6 and cjs packages

---
+++ tenant-landlord-app/jest.config.js
@@ -0,0 +1,14 @@
+const config = {
+    displayName: "react-b",
+    testEnvironment: "jsdom",
+    transform: {
+        "^.+\\.(js|jsx)$": "babel-jest"
+    },
+    transformIgnorePatterns: ["/node_modules/(?!react-auth-kit|axios|bootstrap|react-icons)"],
+    moduleFileExtensions: ["js", "jsx", "json", "node"],
+    moduleNameMapper: {
+        "\\.(css|less|scss)$": "identity-obj-proxy",
+      },
+  };
+
+export default config;
--- tenant-landlord-app/src/__tests__/FirstTest.test.js
+++ tenant-landlord-app/src/__tests__/FirstTest.test.js
@@ -1,17 +1,27 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import FirstTest from "../component_test/FirstTest";
+import { render,  screen } from "@testing-library/react";
+import FirstTest from "../component_test/FirstTest.js";
 import CreateTicketPage from "../pages/CreateTicketPage";
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from "../pages/App.js";
+import '@testing-library/jest-dom'
 test("Testing the set up file", () => {
   render(<FirstTest />);
 
+  
   const element = screen.getByText(/first test/i);
 
   expect(element).toBeInTheDocument();
 });
 
+test ("dummy test", () => {
+  expect()
+})
+
 test("Render Create Ticket Page", () => {
-  render(<App />);
+  render(    
+  <Router>
+    <App />
+  </Router>);
   //expect(screen.getByText(/Create A Service Ticket/i)).toBeInTheDocument();
 });
