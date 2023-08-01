ee3cad1a8f04b4236fe8ac0b7389ccc02a3d2350
Jovi Tan <xxpurelistxx@gmail.com>
2023-08-01 10:23:21 +0800

    chore: removed unneeded pages

--- tenant-landlord-app/src/pages/App.js
+++ tenant-landlord-app/src/pages/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import LoginPage from './LoginPage.js';
 import LandlordSignup from './landlord_signup.js';
 import Dashboard from './Dashboard.js';
-import CloseTicketPage from './CloseTicketPage.js';
 import FeedbackForm from './FeedbackForm.js';
 import CreateTicketPage from './CreateTicketPage.js'; 
 import ViewTicketPage from './ViewTicketPage.js';
@@ -38,7 +37,6 @@ function App() {
             <Route path="/pages/landlord_signup/*" element={<LandlordSignup />} />
             <Route path='/pages/Dashboard/*' element={<Dashboard/>} />
             <Route path='/pages/TicketList/*' element={<TicketList/>} />
-            <Route path='/pages/CloseTicketPage/*' element={<CloseTicketPage/>} />
             <Route path='/pages/FeedbackForm/*' element={<FeedbackForm/>} />
             <Route path='/pages/CreateTicketPage/*' element={<CreateTicketPage/>} />
             <Route path='/pages/ViewTicketPage/*' element={<ViewTicketPage/>} />
