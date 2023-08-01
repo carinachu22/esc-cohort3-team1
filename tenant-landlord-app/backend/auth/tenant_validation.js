2b90327a6316ca24d275cfe153a749064dc57d8b
shinz2307 <127090101+shinz2307@users.noreply.github.com>
2023-08-01 10:52:52 +0800

    feat: return validation status

--- tenant-landlord-app/backend/auth/tenant_validation.js
+++ tenant-landlord-app/backend/auth/tenant_validation.js
@@ -6,7 +6,7 @@ export const checkTenantToken = (req, res, next) => {
         token = token.slice(7);
         verify(token, "paolom8", (error, decoded) => {
         if (error) {
-            res.json({
+            return res.json({
             success: 0,
             message: "Invalid token",
             });
@@ -15,7 +15,7 @@ export const checkTenantToken = (req, res, next) => {
         }
         });
     } else {
-        res.json({
+        return res.json({
         success: 0,
         message: "Access denied: You are unauthorized!",
         });
