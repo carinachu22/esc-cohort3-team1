83ceef1cd889ba1d44e165ede376170db6d330cd
cnjoanne <scn153@gmail.com>
2023-08-03 17:18:35 +0800

    Update: improve e2e test login (ten and ll)

---
+++ tenant-landlord-app/endtoend_test/test/alt_test_login.spec.js
@@ -0,0 +1,59 @@
+const {By, Builder, Browser, until} = require('selenium-webdriver');
+const chai = require('chai');
+
+
+// ASSUMING THE SERVICE REQUEST TABLE IS EMPTY
+const assert = chai.assert;
+describe('Unusual Login', function () {
+    let tenant_driver;
+    let landlord_driver
+
+    before(async function () {
+    landlord_driver = await new Builder().forBrowser('chrome').build();
+    });
+
+    after(async () => { await landlord_driver.quit()});
+    
+    it('Select Landlord Option', async function () {
+        await driver.get('http://localhost:3000/');
+
+        let title = await driver.getTitle();
+        assert.equal("React App", title);
+
+        await driver.manage().setTimeouts({ implicit: 300 });
+
+        // In first page
+        await driver.findElement(By.xpath("//*[@id='root']/div/div/div/div/div/button[1]")).click();
+
+        await driver.manage().setTimeouts({ implicit: 500 });
+
+        // In Landlord Login Page
+        let result = await driver.findElement(By.xpath("//*[@id='root']/div/div/div/div/form/div/h2")).isDisplayed();
+        assert.equal(true, result);
+    });
+
+    // it('Landlord login', async function () {
+    //     await driver.get('http://localhost:3000/pages/LoginPage');
+
+    //     let title = await driver.getTitle();
+    //     assert.equal("React App", title);
+
+    //     await driver.manage().setTimeouts({ implicit: 300 });
+
+    //     await driver.findElement(By.xpath('//*[@id="email"]')).sendKeys('landlord1@gmail.com');
+    //     await driver.findElement(By.xpath('//*[@id="password"]')).sendKeys('password');
+    //     await driver.findElement(By.xpath('//*[@id="root"]/div/div/div/div/form/div/div[3]')).click();
+
+    //     await driver.manage().setTimeouts({ implicit: 800 });
+
+    //     // In Landlord Dashboard
+    //     let result = await driver.findElement(By.xpath("//*[@id='root']/div/div/div/div/form/div/div[1]/div")).isDisplayed();
+    //     assert.equal(true, result);
+    // });
+
+
+
+
+
+
+});
