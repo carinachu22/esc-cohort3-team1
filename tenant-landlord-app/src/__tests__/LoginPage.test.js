0a78e981a4edd351d30fbfe6c864ec785fbc2a4c
Carina <carinachu22@gmail.com>
2023-08-04 01:09:13 +0800

    test: test case for sucessful login api call

--- tenant-landlord-app/src/__tests__/LoginPage.test.js
+++ tenant-landlord-app/src/__tests__/LoginPage.test.js
@@ -11,10 +11,12 @@ import {
 } from "react-router-dom";
 import { AuthProvider, useSignIn } from "react-auth-kit";
 import { useFormik } from "formik";
-//import axios, { AxiosError } from "axios";
+import axios, { AxiosError } from "axios";
 
 import LoginPage from "../pages/LoginPage.js";
 import Dashboard from "../pages/Dashboard.js";
+
+jest.mock("axios");
 
 describe("Login.js", () => {
   test("Email,Password, login btn features are present", () => {
@@ -60,7 +62,54 @@ describe("Login.js", () => {
     expect(submitBtn).toBeInTheDocument();
   });
 
-  test("Toggle Password visible", async () => {
+  test("Valid email and password successfuly calls login API", async () => {
+    render(
+      <AuthProvider
+        authType={"cookie"}
+        authName={"_auth"}
+        cookieDomain={window.location.hostname}
+        cookieSecure={false}
+      >
+        <BrowserRouter>
+          <LoginPage />
+        </BrowserRouter>
+      </AuthProvider>
+    );
+    const resp_data = [
+      {
+        tenant_user_id: 38,
+        email: "tenant1@gmail.com",
+        password:
+          "$2b$10$muetmRj1fDH.93XKBFt2yO.GMeLlDJLzB5fniGDLb/0lRNLjzb80y",
+        public_building_id: "RC",
+        public_lease_id: "1690926182158",
+        deleted_date: null,
+      },
+    ];
+    const resp = { data: resp_data };
+    const values = {
+      email: "tenant1@gmail.com",
+      hasError: false,
+      password: "password",
+    };
+    await userEvent.type(
+      screen.getByPlaceholderText("Email"),
+      "tenant1@gmail.com"
+    );
+    expect(screen.getByPlaceholderText("Email")).toHaveValue(
+      "tenant1@gmail.com"
+    );
+    await userEvent.type(screen.getByPlaceholderText("Password"), "password");
+    expect(screen.getByPlaceholderText("Password")).toHaveValue("password");
+    axios.post.mockResolvedValue(resp);
+    await userEvent.click(screen.getByRole("button", { name: "LOGIN" }));
+    expect(axios.post).toHaveBeenCalledWith(
+      "http://localhost:5000/api/tenant/login",
+      values
+    );
+  });
+
+  test("Show button clicked, Toggle Password visible", async () => {
     render(
       <AuthProvider
         authType={"cookie"}
