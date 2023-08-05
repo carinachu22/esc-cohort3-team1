cf42eb416872daecfeb16da2ddd764303672698a
shinz2307 <127090101+shinz2307@users.noreply.github.com>
2023-08-05 14:36:26 +0800

    test: landlord model tests
    all 28 test cases should pass with final_integration version from 4 aug

--- tenant-landlord-app/backend/__test__/models/landlord_model.test.js
+++ tenant-landlord-app/backend/__test__/models/landlord_model.test.js
@@ -97,3 +97,354 @@ describe ("Testing getTicketsByStatus() in landlord model", () => {
         })
     })
 })
+
+describe("Testing getLandlordById() in landlord model", () => {
+    test ("Test calling getLandlordById() on a valid landlord ID",(done) => {
+        getLandlordById(1, (err, results) => {
+            if (err){
+                console.log("ERROR",err)
+            }
+            const rowsLength = results.length
+            expect(rowsLength).toBe(1);
+            done();
+        })
+    });
+    test ("Test calling getLandlordById() on an invalid landlord ID",(done) => {
+        getLandlordById(999, (err, results) => {
+            if (err){
+                console.log("ERROR",err)
+            }
+            const rowsLength = results.length
+            expect(rowsLength).toBe(0);
+            done();
+        })
+    });
+})
+
+describe("Testing updateLandlordPassword() in landlord model", () => {
+    test ("Test calling updateLandlordPassword() on valid landlord ID",(done) => {
+        const data = {
+            password: "$2b$10$BIJTkvtOrkrKhl/juVKCBuVhPwqChMNbayD3DazrMBi6H6gsgVlrS",
+            id: 1
+        }
+        updateLandlordPassword(data, (err, results) => {
+            if (err){
+                console.log("ERROR",err)
+            }
+            const rowsChanged = JSON.parse(JSON.stringify(results)).changedRows
+            expect(rowsChanged).toBe(1);
+            done();
+        })
+    });
+    test ("Test calling updateLandlordPassword() on an invalid landlord ID",(done) => {
+        const data = {
+            password: "$2b$10$BIJTkvtOrkrKhl/juVKCBuVhPwqChMNbayD3DazrMBi6H6gsgVlrS",
+            id: 999
+        }
+        updateLandlordPassword(data, (err, results) => {
+            if (err){
+                console.log("ERROR",err)
+            }
+            const rowsChanged = JSON.parse(JSON.stringify(results)).changedRows
+            expect(rowsChanged).toBe(0);
+            done();
+        })
+    });
+})
+
+describe("Testing getLandlordUserId() in landlord model", () => {
+    test ("Test calling getLandlordUserId() on a valid email",(done) => {
+        getLandlordUserId("landlord1@gmail.com", (err, results) => {
+            if (err){
+                console.log("ERROR",err)
+            }
+            const rowsLength = results.length
+            expect(rowsLength).toBe(1);
+            done();
+        })
+    });
+    test ("Test calling getLandlordUserId() on an invalid email",(done) => {
+        getLandlordUserId("landlord@gmail.com", (err, results) => {
+            if (err){
+                console.log("ERROR",err)
+            }
+            const rowsLength = results.length
+            expect(rowsLength).toBe(0);
+            done();
+        })
+    });
+})
+
+describe("testing createLandlord() in landlord model", () => {
+    test ("Test calling createLandlord() with valid values",(done) => {
+        const data = {
+            email: 'landlord4@gmail.com',
+            password: '$2b$10$BIJTkvtOrkrKhl/juVKCauVhPwqChMNbayD3DazrMBi6H6gsgVlrS',
+            ticket_type: 'cleanliness'
+        }
+        createLandlord(data, (err, results) => {
+            if (err){
+                console.log("ERROR",err)
+            }
+            console.log(`results ${results}`)
+            const rowsChanged = JSON.parse(JSON.stringify(results)).affectedRows
+            expect(rowsChanged).toBe(1);
+            done();
+        })
+    });
+
+    test ("Test calling createLandlord() with missing email",(done) => {
+        const data = {
+            password: '$2b$10$BIJTkvtOrkrKhl/juVKCauVhPwqChMNbayD3DazrMBi6H6gsgVlrS',
+            ticket_type: 'cleanliness'
+        }
+        createLandlord(data, (err, results) => {
+            expect(err).toBe("missing data entry!");
+            done();
+        })
+    });
+
+    test ("Test calling createLandlord() with missing password",(done) => {
+        const data = {
+            email: 'landlord4@gmail.com',
+            ticket_type: 'cleanliness'
+        }
+        createLandlord(data, (err, results) => {
+            expect(err).toBe("missing data entry!");
+            done();
+        })
+    });
+
+    test ("Test calling createLandlord() with missing ticket_type",(done) => {
+        const data = {
+            email: 'landlord4@gmail.com',
+            password: '$2b$10$BIJTkvtOrkrKhl/juVKCauVhPwqChMNbayD3DazrMBi6H6gsgVlrS',
+        }
+        createLandlord(data, (err, results) => {
+            expect(err).toBe("missing data entry!");
+            done();
+        })
+    });
+
+    test ("Test calling createLandlord() with duplicate email",(done) => {
+        const data = {
+            email: 'landlord1@gmail.com',
+            password: '$2b$10$BIJTkvtOrkrKhl/juVKCauVhPwqChMNbayD3DazrMBi6H6gsgVlrS',
+            ticket_type: 'horticuture'
+        }
+        createLandlord(data, (err, results) => {
+            expect(err).toBe("landlord user already exists");
+            done();
+        })
+    });
+})
+
+describe("Testing deleteAllTenants() in landlord model", () => {
+    test("Test calling deleteAllTenants() with valid public building id", (done) => {
+        deleteAllTenants(Date.now(), 'RC', (err,results) => {
+            if (err){
+                console.log("ERROR",err)
+            }
+            console.log(`results ${results}`)
+            const rowsChanged = JSON.parse(JSON.stringify(results)).affectedRows
+            expect(rowsChanged).toBe(2);
+            done();
+        })
+    })
+
+    test("Test calling deleteAllTenants() with invalid public building id", (done) => {
+        deleteAllTenants(Date.now(), 'ABC', (err,results) => {
+            if (err){
+                console.log("ERROR",err)
+            }
+            console.log(`results ${results}`)
+            const rowsChanged = JSON.parse(JSON.stringify(results)).affectedRows
+            expect(rowsChanged).toBe(0);
+            done();
+        })
+    })
+})
+
+describe("Testing deleteTenantByEmail() in landlord model", () => {
+    test("Test calling deletetenantByEmail() with valid email", (done) => {
+        deleteTenantByEmail(Date.now(), 'tenant1@gmail.com', (err,results) => {
+            if (err){
+                console.log("ERROR",err)
+            }
+            console.log(`results ${results}`)
+            const rowsChanged = JSON.parse(JSON.stringify(results)).affectedRows
+            expect(rowsChanged).toBe(1);
+            done();
+        })
+    })
+
+    test("Test calling deleteTenantByEmail() with invalid email", (done) => {
+        deleteTenantByEmail(Date.now(), 'random@gmail.com', (err,results) => {
+            if (err){
+                console.log("ERROR",err)
+            }
+            console.log(`results ${results}`)
+            const rowsChanged = JSON.parse(JSON.stringify(results)).affectedRows
+            expect(rowsChanged).toBe(0);
+            done();
+        })
+    })
+})
+
+describe("testing createTenant() in landlord model", () => {
+    test ("Test calling createTenant() with valid values",(done) => {
+        const data = {
+            email: 'tenant10@gmail.com',
+            password: '$2b$10$BIJTkvtOrkrKhl/juVKCauVhPwqChMNbayD3DazrMBi6H6gsgVlrS',
+            public_building_id: 'CWP'
+        }
+        createTenant(data.email, data.password, data.public_building_id, (err, results) => {
+            if (err){
+                console.log("ERROR",err)
+            }
+            console.log(`results ${results}`)
+            const rowsChanged = JSON.parse(JSON.stringify(results)).affectedRows
+            expect(rowsChanged).toBe(1);
+            done();
+        })
+    });
+
+    test ("Test calling createTenant() with missing email",(done) => {
+        const data = {
+            password: '$2b$10$BIJTkvtOrkrKhl/juVKCauVhPwqChMNbayD3DazrMBi6H6gsgVlrS',
+            public_building_id: "CWP"
+        }
+        createTenant(data.email, data.password, data.public_building_id, (err, results) => {
+            expect(err).toBe("missing data entry!");
+            done();
+        })
+    });
+
+    test ("Test calling createTenant() with missing password",(done) => {
+        const data = {
+            email: 'tenant10@gmail.com',
+            public_building_id: "CWP"
+        }
+        createTenant(data.email, data.password, data.public_building_id, (err, results) => {
+            expect(err).toBe("missing data entry!");
+            done();
+        })
+    });
+
+    test ("Test calling createTenant() with missing public_building_id",(done) => {
+        const data = {
+            email: 'tenant10@gmail.com',
+            password: '$2b$10$BIJTkvtOrkrKhl/juVKCauVhPwqChMNbayD3DazrMBi6H6gsgVlrS',
+        }
+        createTenant(data.email, data.password, data.public_building_id, (err, results) => {
+            expect(err).toBe("missing data entry!");
+            done();
+        })
+    });
+
+    test ("Test calling createTenant() with duplicate email",(done) => {
+        const data = {
+            email: 'tenant1@gmail.com',
+            password: '$2b$10$BIJTkvtOrkrKhl/juVKCauVhPwqChMNbayD3DazrMBi6H6gsgVlrS',
+            public_building_id: "CWP"
+        }
+        createTenant(data.email, data.password, data.public_building_id, (err, results) => {
+            expect(err).toBe("tenant user already exists");
+            done();
+        })
+    });
+})
+
+describe("testing getTickets() in landlord model", () => {
+    test ("Test calling getTickets()",(done) => {
+        getTickets((err, results) => {
+            if (err){
+                console.log("ERROR",err)
+            }
+            expect(results.length).toBe(5);
+            done();
+        })
+    });
+})
+
+describe("Testing updateQuotation() in Landlord model", () => {
+    test ("Test calling updateLandlordPassword() on valid public ticket ID",(done) => {
+        updateQuotation("SR/2002/Feb/0001", (err, results) => {
+            if (err){
+                console.log("ERROR",err)
+            }
+            const rowsChanged = JSON.parse(JSON.stringify(results)).changedRows
+            expect(rowsChanged).toBe(1);
+            done();
+        })
+    });
+    test ("Test calling updateQuotation() on an invalid public ticket ID",(done) => {
+        updateQuotation("SR/9999/999/9999", (err, results) => {
+            if (err){
+                console.log("ERROR",err)
+            }
+            const rowsChanged = JSON.parse(JSON.stringify(results)).changedRows
+            expect(rowsChanged).toBe(0);
+            done();
+        })
+    });
+})
+
+// //TODO: getLeaseByLandlord
+// describe("Testing getLeaseByLandlord() in landlord model", () => {
+//     test ("Test calling getLeaseByLandlord() on valid landlord id",(done) => {
+//         getLeaseByLandlord(4, (err, results) => {
+//             if (err){
+//                 console.log("ERROR",err)
+//             }
+//             const rowsChanged = JSON.parse(JSON.stringify(results)).changedRows
+//             expect(rowsChanged).toBe(1);
+//             done();
+//         })
+//     });
+//     test ("Test calling getLeaseByLandlord() on an invalid landlord id",(done) => {
+//         getLeaseByLandlord(999, (err, results) => {
+//             if (err){
+//                 console.log("ERROR",err)
+//             }
+//             const rowsChanged = JSON.parse(JSON.stringify(results)).changedRows
+//             expect(rowsChanged).toBe(0);
+//             done();
+//         })
+//     });
+// })
+
+
+// TODO: updateLandlord
+
+//TODO: deleteLandlord
+
+//TODO: getTicketsById
+
+//TODO: uploadQuotation
+
+//TODO: getQuotationPath
+
+//TODO: getQuotation
+
+//TODO: ticketApproval
+
+//TODO: ticketWork
+
+//TODO: getTenantAccounts
+
+//TODO:createLease
+
+//TODO: uploadLease
+
+//TODO: getLeaseDetails
+
+//TODO: deleteLease
+
+//TODO: updateLease
+
+//TODO: getLeasePath
+
+//TODO: getLease
+
+//TODO: getBuildings
